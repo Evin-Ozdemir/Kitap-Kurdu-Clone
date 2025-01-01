88fdd302ff134ac47ced968b451d907763abe19b
Evin Özdemir <evinngulcan@gmail.com>
2025-01-02 02:31:33 +0300

    missing pieces completed

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Card = ({ book }) => {
   return (
     <div className="card shadow p-2">
-      <img src={book.image} className="rounded" />
+      <img src={book.image} className=" rounded " />
       <div className="card-body">
         <h4>{book.title}</h4>
         <h4>{book.author}</h4>
